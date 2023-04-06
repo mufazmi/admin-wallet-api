ed16f983ab6157b01510e17a2f4a1391e9c2de8d
Umair Farooqui <info.mufazmi@gmail.com>
2023-04-06 22:06:06 +0530

    implemented : Get Single & Multiple Message template

--- src/controllers/message-template-controller.ts
+++ src/controllers/message-template-controller.ts
@@ -1,4 +1,4 @@
-import {Request,Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express"
 import otpTemplateValidation from "../validations/message-template-validation"
 import responseSuccess from "../utils/response";
 import Constants from "../utils/constants";
@@ -9,32 +9,38 @@ import messageTemplateService from "../services/message-template-service";
 
 class MessageTemplateController {
 
-    create = async (req:Request,res:Response,next:NextFunction) => {
+    create = async (req: Request, res: Response, next: NextFunction) => {
         const body = await otpTemplateValidation.create.validateAsync(req.body);
         const data = await messageTemplateService.create(body);
-        return data ? responseSuccess({res:res,message:Messages.MESSAGE.TEMPLATE_MESSAGE_CREATED}) : next(ErrorHandler.serverError(Messages.MESSAGE.TEMPLATE_MESSAGE_CREATION_FAILED));
+        return data ? responseSuccess({ res: res, message: Messages.MESSAGE.TEMPLATE_MESSAGE_CREATED }) : next(ErrorHandler.serverError(Messages.MESSAGE.TEMPLATE_MESSAGE_CREATION_FAILED));
     }
 
-    update = async (req:Request,res:Response,next:NextFunction) => {
-        const {id} = req.params;
+    find = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+        const data = await messageTemplateService.findAll({ id });
+        return data ? responseSuccess({ res: res, message: Messages.MESSAGE.TEMPLATE_MESSAGE_FOUND, data: data }) : next(ErrorHandler.notFound(Messages.MESSAGE.TEMPLATE_MESSAGE_NOT_FOUND));
+
+    }
+
+    findAll = async (req: Request, res: Response, next: NextFunction) => {
+        const data = await messageTemplateService.findAll({});
+        return data ? responseSuccess({ res: res, message: Messages.MESSAGE.TEMPLATE_MESSAGE_FOUND, data: data }) : next(ErrorHandler.notFound(Messages.MESSAGE.TEMPLATE_MESSAGE_NOT_FOUND));
+    }
+
+    update = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
         const body = await otpTemplateValidation.update.validateAsync(req.body);
-        const template = await messageTemplateService.find({id});
-        if(!template)
+        const template = await messageTemplateService.find({ id });
+        if (!template)
             return next(ErrorHandler.notFound(Messages.MESSAGE.TEMPLATE_MESSAGE_NOT_FOUND))
-        
-        const data = await messageTemplateService.update({id},body);
-        return data ? responseSuccess({res:res,message:Messages.MESSAGE.TEMPLATE_MESSAGE_UPDATED}) : next(ErrorHandler.serverError(Messages.MESSAGE.TEMPLATE_MESSAGE_UPDATE_FAILED));
+
+        const data = await messageTemplateService.update({ id }, body);
+        return data ? responseSuccess({ res: res, message: Messages.MESSAGE.TEMPLATE_MESSAGE_UPDATED }) : next(ErrorHandler.serverError(Messages.MESSAGE.TEMPLATE_MESSAGE_UPDATE_FAILED));
     }
 
-    find = async (req:Request,res:Response,next:NextFunction) => {
 
-    }
 
-    findAll = async (req:Request,res:Response,next:NextFunction) => {
-
-    }
-    
-    destroy = async (req:Request,res:Response,next:NextFunction) => {
+    destroy = async (req: Request, res: Response, next: NextFunction) => {
 
     }
 }
