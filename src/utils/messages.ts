14e051ce0417a2760a32b91eb62d0d86bd5ed007
Umair Farooqui <info.mufazmi@gmail.com>
2023-04-07 03:28:07 +0530

    Created & Implemented : Country, City, State & Notification Token Controllers

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -13,16 +13,46 @@ class Messages {
     }
 
 
-    static MESSAGE = {
-        TEMPLATE_MESSAGE_CREATED: 'Template Created Successfully',
-        TEMPLATE_MESSAGE_CREATION_FAILED: 'Failed To Create Template',
-        TEMPLATE_MESSAGE_FOUND: 'Message Template Found',
-        TEMPLATE_MESSAGE_NOT_FOUND: 'No Message Template Found',
-        TEMPLATE_MESSAGE_UPDATED: 'Message Template Updated',
-        TEMPLATE_MESSAGE_UPDATE_FAILED: 'Failed To Updated Message Template',
-        TEMPLATE_MESSAGE_DELATED: 'Message Template Deleted',
-        TEMPLATE_MESSAGE_DELETE_FAILED: 'Failed To Delete Message Template'
+    static COUNTRY = {
+        COUNTRY_CREATED: 'Country Created Successfully',
+        COUNTRY_CREATION_FAILED: 'Failed To Create Country',
+        COUNTRY_FOUND: 'Country Found',
+        COUNTRY_NOT_FOUND: 'No Country Found',
+        COUNTRY_UPDATED: 'Country Updated',
+        COUNTRY_UPDATE_FAILED: 'Failed To Updated Country',
+        COUNTRY_DELATED: 'Country Deleted',
+        COUNTRY_DELETE_FAILED: 'Failed To Delete Country'
     }
+
+    static STATE = {
+        STATE_CREATED: 'State Created Successfully',
+        STATE_CREATION_FAILED: 'Failed To Create State',
+        STATE_FOUND: 'State Found',
+        STATE_NOT_FOUND: 'No State Found',
+        STATE_UPDATED: 'State Updated',
+        STATE_UPDATE_FAILED: 'Failed To Updated State',
+        STATE_DELATED: 'State Deleted',
+        STATE_DELETE_FAILED: 'Failed To Delete State'
+    }
+
+    static CITY = {
+        CITY_CREATED: 'City Created Successfully',
+        CITY_CREATION_FAILED: 'Failed To Create City',
+        CITY_FOUND: 'City Found',
+        CITY_NOT_FOUND: 'No City Found',
+        CITY_UPDATED: 'City Updated',
+        CITY_UPDATE_FAILED: 'Failed To Updated City',
+        CITY_DELATED: 'City Deleted',
+        CITY_DELETE_FAILED: 'Failed To Delete City'
+    }
+
+    static NOTIFICATION = {
+        NOTIFICATION_TOKEN_CREATED: 'Notification Token Created Successfully',
+        NOTIFICATION_TOKEN_CREATION_FAILED: 'Failed To Create Notification Token',
+        NOTIFICATION_TOKEN_DELATED: 'Notification Token Deleted',
+        NOTIFICATION_TOKEN_DELETE_FAILED: 'Failed To Delete Notification Token'
+    }
+
 
 }
 
